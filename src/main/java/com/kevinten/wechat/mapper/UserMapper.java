9df7e5af00716dcb520d4099fe2ad884ad3d04b7
kevinten10 <596823919@qq.com>
2018-07-25 13:55:12 +0800

    init

--- src/main/java/com/kevinten/wechat/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.kevinten.wechat.mapper;
-
-import com.kevinten.wechat.entity.UserDO;
-
-import java.util.List;
-
-public interface UserMapper {
-    List<UserDO> queryUserList();
-}
--- src/main/webapp/index.jsp
+++
@@ -1,9 +0,0 @@
-<html>
-<head>
-    <meta charset="UTF-8">
-    <title>wechat api</title>
-</head>
-<body>
-<h2>Hello World!</h2>
-</body>
-</html>
