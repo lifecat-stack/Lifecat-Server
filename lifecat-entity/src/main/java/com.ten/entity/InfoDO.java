7caf42caac00dfe0ac6fa47b630cb0181aaddbbf
ten <596823919@qq.com>
2018-07-26 09:29:56 +0800

    version1.0.0   --init

--- lifecat-entity/src/main/java/com.ten/entity/InfoDO.java
+++ lifecat-entity/src/main/java/com.ten/entity/InfoDO.java
@@ -1,4 +1,22 @@
 package com.ten.entity;
 
 public class InfoDO {
+    private Integer albumId;
+    private Integer userId;
+    private String albumName;
+    private String albumDesc;
+    private String albumCoverUrl;
+    private boolean deleted;
+    private String albumGmtCreate;
+    private String albumGmtModified;
+
+    @Override
+    public String toString() {
+        return "c_album@{" +
+                "album_id:" + albumId +
+                ",user_id:" + userId +
+                ",album_name:" + albumName +
+                ",album_desc:" + albumDesc +
+    }
+
 }
