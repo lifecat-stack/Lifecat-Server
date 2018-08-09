3a3abed13e532ca9a581831b5aa6129a062dc88d
kevinten10 <596823919@qq.com>
2018-08-09 10:48:32 +0800

    version3.1.0   --2018.8.9   --junit测试springboot

---
+++ lifecat-web/src/test/java/com/ten/controller/TestControllerTest.java
@@ -0,0 +1,103 @@
+package com.ten.controller;
+
+import javafx.application.Application;
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * TestController Tester.
+ *
+ * @author <Authors name>
+ * @version 1.0
+ * @since <pre>���� 9, 2018</pre>
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = Application.class)
+public class TestControllerTest {
+
+    @Before
+    public void before() throws Exception {
+    }
+
+    @After
+    public void after() throws Exception {
+    }
+
+    /**
+     * Method: all()
+     */
+    @Test
+    public void testAll() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: listById(@PathVariable String testId)
+     */
+    @Test
+    public void testListById() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: list(@RequestBody TestVO entity)
+     */
+    @Test
+    public void testList() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: getById(@PathVariable String testId)
+     */
+    @Test
+    public void testGetById() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: get(@RequestBody TestVO entity)
+     */
+    @Test
+    public void testGet() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: post(@RequestBody TestVO entity)
+     */
+    @Test
+    public void testPost() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: put(@RequestBody TestVO entity)
+     */
+    @Test
+    public void testPut() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: deleteById(@PathVariable String testId)
+     */
+    @Test
+    public void testDeleteById() throws Exception {
+//TODO: Test goes here... 
+    }
+
+    /**
+     * Method: delete(@RequestBody TestVO entity)
+     */
+    @Test
+    public void testDelete() throws Exception {
+//TODO: Test goes here... 
+    }
+
+
+} 
